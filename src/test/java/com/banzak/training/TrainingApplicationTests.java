078a2ba74ef38e0545cdca0452451b0340a01e4a
banzak1 <leosantanabr@gmail.com>
2023-04-19 12:05:58 -0300

    inicializando projeto

---
+++ src/test/java/com/banzak/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banzak.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
