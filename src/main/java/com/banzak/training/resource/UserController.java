f72dca1da3395c3bc1144e0b4c34753702241d67
banzak1 <leosantanabr@gmail.com>
2023-04-19 11:54:56 -0300

    Criando os controllers da classe usuário.

---
+++ src/main/java/com/banzak/training/resource/UserController.java
@@ -0,0 +1,43 @@
+package com.banzak.training.resource;
+
+import com.banzak.training.model.User;
+import com.banzak.training.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUser() {
+        return userService.findAllUser();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(@PathVariable Long id, @RequestBody User user) {
+        userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleterUser(id);
+    }
+
+}
