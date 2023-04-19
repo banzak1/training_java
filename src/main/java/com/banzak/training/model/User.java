0dc409548db50be20f61862296b04e07462ba4c5
banzak1 <leosantanabr@gmail.com>
2023-04-19 11:54:31 -0300

    Criando models de usuário.

---
+++ src/main/java/com/banzak/training/model/User.java
@@ -0,0 +1,24 @@
+package com.banzak.training.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
